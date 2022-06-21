d8fbf6829c3d5a0280950d7e5764012c19075f68
hyunmin <choihm9903@naver.com>
2022-06-21 12:31:29 +0900

    feat: todo-list

--- 05_DRFTodo/config/settings.py
+++ 05_DRFTodo/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^y564nxluv_e=ff(h4!juba2c#vujk44j)&qn+9ezsx*j6x@al
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
