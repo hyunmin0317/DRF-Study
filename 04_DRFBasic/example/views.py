015c4075a447c135686815dee9fb7e5e82032511
hyunmin <choihm9903@naver.com>
2022-06-21 11:43:04 +0900

    feat: model-Book

--- 04_DRFBasic/example/views.py
+++ 04_DRFBasic/example/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response("hello world!")
+
+
+class HelloAPI(APIView):
+    def get(self, request):
+        return Response("hello world")
