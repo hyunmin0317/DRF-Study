63415d2c43f1f6246274b2e3fd987cf8aca9abdb
hyunmin <choihm9903@naver.com>
2022-06-21 11:58:28 +0900

    feat: FBV

--- 04_DRFBasic/example/urls.py
+++ 04_DRFBasic/example/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from example.views import HelloAPI
+from example.views import HelloAPI, booksAPI, bookAPI, BooksAPI, BookAPI, helloAPI
 
 urlpatterns = [
-    path('hello/', HelloAPI),
+    path("fbv/hello/", helloAPI),
+    path("fbv/books/", booksAPI),
+    path("fbv/book/<int:bid>/", bookAPI),
+    path("cbv/hello/", HelloAPI.as_view()),
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
 ]
--- 04_DRFBasic/example/views.py
+++ 04_DRFBasic/example/views.py
@@ -1,12 +1,36 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from example.models import Book
+from example.serializers import BookSerializer
 
+
+# FBV
 @api_view(['GET'])
-def HelloAPI(request):
+def helloAPI(request):
     return Response("hello world!")
 
 
-class HelloAPI(APIView):
-    def get(self, request):
-        return Response("hello world")
+@api_view(['GET', 'POST'])
+def booksAPI(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def bookAPI(request, bid):
+    book = get_object_or_404(Book, bid=bid)
+    serializer = BookSerializer(book)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
