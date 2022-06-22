91f71ffb103b391bfb0df394e265aece48281c6a
hyunmin <choihm9903@naver.com>
2022-06-22 10:54:23 +0900

    feat: login

--- 06_BoardProject/users/views.py
+++ 06_BoardProject/users/views.py
@@ -1,8 +1,21 @@
 from django.contrib.auth.models import User
-from rest_framework.generics import CreateAPIView
-from users.serializers import RegisterSerializer
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.response import Response
+
+from users.serializers import RegisterSerializer, LoginSerializer
 
 
 class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class LoginView(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
