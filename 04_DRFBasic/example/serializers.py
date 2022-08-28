6817f7a59a0a54ba4c54057d806e8b65264ffa2b
hyunmin <choihm9903@naver.com>
2022-08-28 16:47:32 +0900

    feat: model-Book

---
+++ 04_DRFBasic/example/serializers.py
@@ -0,0 +1,8 @@
+from rest_auth.app_settings import serializers
+from example.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['bid', 'title', 'author', 'category', 'pages', 'price', 'published_date', 'description']
