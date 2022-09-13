0dbc47d7cc4175399289235f78107b55d6e1add6
hyunmin <choihm9903@naver.com>
2022-09-13 14:13:29 +0900

    feat: project

--- 05_DRFTodo/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from todo.views import TodoAPIView, TodosAPIView, DoneTodoAPIView, DoneTodosAPIView
-
-urlpatterns = [
-    path('todo/', TodosAPIView.as_view()),
-    path('todo/<int:pk>/', TodoAPIView.as_view()),
-    path('done/', DoneTodosAPIView.as_view()),
-    path('done/<int:pk>/', DoneTodoAPIView.as_view()),
-]
