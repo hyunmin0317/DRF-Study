58b02e079543134518ad5569b86363c2fc6bedc4
hyunmin <choihm9903@naver.com>
2022-09-18 16:51:44 +0900

    feat: app-posts

--- 06_BoardProject/config/settings.py
+++ 06_BoardProject/config/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'users',
-    'corsheaders'
+    'posts',
 ]
 
 MIDDLEWARE = [
