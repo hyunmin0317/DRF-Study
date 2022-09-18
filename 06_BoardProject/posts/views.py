aa50af0f554f33dccefde910a2791db9465a3f9f
hyunmin <choihm9903@naver.com>
2022-09-18 17:36:23 +0900

    feat: model-views

--- 06_BoardProject/posts/views.py
+++ 06_BoardProject/posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from posts.models import Post
+from posts.permissions import CustomReadOnly
+from posts.serializers import PostSerializer, PostCreateSerializer
+from users.models import Profile
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    permission_classes = [CustomReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == 'list' or 'retrieve':
+            return PostSerializer
+        return PostCreateSerializer
+
+    def perform_create(self, serializer):
+        profile = Profile.objects.get(user=self.request.user)
+        serializer.save(author=self.request.user, profile=profile)
+        
